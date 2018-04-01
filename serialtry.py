1e410e6dac1eaed83706adf895e4ce84166d53c8
jeffitoro <jeff-oro@hotmail.com>
2018-04-01 15:11:54 +0000

    last modif

--- serialtry.py
+++ serialtry.py
@@ -7,43 +7,53 @@ import time
 
 serialport = serial.Serial("/dev/ttyACM0",9600,timeout=0.5)
 url ='https://api.thingspeak.com/apps/thinghttp/send_request?api_key=VQX7ZL0145XOB43J'
+urlGet = 'https://api.thingspeak.com/talkbacks/24302/commands/execute.json?api_key=GVDMQ1NQ229NDJBV'
 headers = {'Content-type': 'application/json'}
-
+Led1 = 'H'
+Led2 = 'T'
+hMax = 50
+hMin = 10
+tMax = 30
+tMin = 18
+namePlant = " l'une de vos plante "
 while True:
+    getInfo = requests.post(urlGet)
+    getInfojson = getInfo.json()
     command = serialport.readline()
     a = command.decode('utf-8')
-    
-    if a:        
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setwarnings(False)
+    print(a[:-2])
+    if len(a[:-2])>38:
+        print(getInfojson)
         jsonresult = json.loads(a[:-2])
-        print(jsonresult)
-        if float (jsonresult["humidity"]) > 50:
-            GPIO.setup(12,GPIO.OUT)
-            GPIO.output(12,GPIO.HIGH)
-            print("LED on 12")
-            time.sleep(2)
-            msg = "humidité trop haute "+str(int(jsonresult["humidity"]))
+        if "command_string" in getInfojson:
+            jsonobjet = json.loads(str(getInfojson["command_string"]))
+            if jsonobjet:
+                hMax = float(jsonobjet["humMax"])
+                hMin = float(jsonobjet["humMin"])
+                tMax = float(jsonobjet["tempMax"])
+                tMin = float(jsonobjet["tempMin"])
+                if "Name" in jsonobjet and jsonobjet["Name"]:
+                    namePlant = jsonobjet["Name"]
+        if "humidity" in jsonresult and (float (jsonresult["humidity"]) > hMax or float (jsonresult["humidity"]) < hMin) :
+            serialport.flush()
+            temp1 = str(Led1)
+            serialport.write(b'H')
+            msg = "L'humidité de "+ str(namePlant) +" n'est pas comprise entre "+str(hMin)+" et "+str(hMax)+", la valeur actuelle est: "+str(int(jsonresult["humidity"]))
             data = {
                 'message': msg
             }
             print(msg)
             response = requests.post(url, data=json.dumps(data), headers=headers)
-        else:
-            GPIO.setup(12,GPIO.OUT)
-            GPIO.output(12,GPIO.LOW)
             
-        if float (jsonresult["temperature"]) > 18:
-            GPIO.setup(13,GPIO.OUT)
-            GPIO.output(13,GPIO.HIGH)
+        if "temperature" in jsonresult and (float (jsonresult["temperature"]) > tMax or float (jsonresult["temperature"]) < tMin):
+            serialport.flush()
+            temp2 = str(Led2)
+            serialport.write(b'T')
             print("LED on 13")
             time.sleep(2)
-            msg = "temperature trop haute "+str(int(jsonresult["temperature"]))
+            msg = "La temperature de "+ str(namePlant) +" n'est pas comprise entre "+str(tMin)+" et "+ str(tMax)+", la valeur actuelle est: "+str(int(jsonresult["temperature"]))
             data = {
                 'message': msg
             }
             print(msg)
             response = requests.post(url, data=json.dumps(data), headers=headers)
-        else:
-            GPIO.setup(13,GPIO.OUT)
-            GPIO.output(13,GPIO.LOW)
